0b9d4aa5adf4b36058175eab1ceb734111e99c15
ekos <heyrebornofficial@gmail.com>
2023-09-06 03:14:33 +0700

    Update XmtpInfoPanel.tsx

--- components/XmtpInfoPanel.tsx
+++ components/XmtpInfoPanel.tsx
@@ -100,7 +100,7 @@ const XmtpInfoPanel = (): JSX.Element => {
           Welcome to SOJU Messenger!
         </div>
         <div className="text-md text-n-300" data-testid="get-started-subheader">
-          Say goodbye to the prying eyes of Big Brother. Say hello to SOJU Messenger!
+          Say goodbye to the prying eyes of Big Brother.
         </div>
       </div>
       <div>
